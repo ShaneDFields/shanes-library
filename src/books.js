e8d94b70c29ff9287fd825c3d49f0fd931071f6c
Shane Fields <82911768+ShaneDFields@users.noreply.github.com>
2023-04-05 17:18:14 -0400

    first push

---
+++ src/books.js
@@ -0,0 +1,55 @@
+function findAuthorById(authors, id) {
+  let result = {};
+  for (let i = 0; i < authors.length; i++) {
+    if (id == authors[i].id) {
+      return authors[i];
+    }
+  }
+  return result;
+}
+
+function findBookById(books, id) {
+  let result = {};
+  for (let i = 0; i < books.length; i++) {
+    if (id == books[i].id) {
+      return books[i];
+    }
+  }
+  return result;
+}
+
+function partitionBooksByBorrowedStatus(books) {
+  const result = books.reduce(
+    (acc, item) => {
+      if (item.borrows.find((b) => b.returned == false)) {
+        acc[0].push(item);
+      } else {
+        acc[1].push(item);
+      }
+      return acc;
+    },
+    [[], []]
+  );
+
+  return result;
+}
+
+function getBorrowersForBook(book, accounts) {
+  let borrowers = [];
+  accounts.map((account) => {
+    book.borrows.find((borrow) => {
+      if (borrow.id === account.id) {
+        account["returned"] = borrow.returned;
+        borrowers.push(account);
+      }
+    });
+  });
+  return borrowers.splice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
